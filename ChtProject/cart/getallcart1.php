55d00990ced0064332b746ce755713ddacf1039b
chatty <bisa8002@gmail.com>
2022-07-29 00:28:23 +0800

    07290028

--- ChtProject/cart/getallcart1.php
+++ ChtProject/cart/getallcart1.php
@@ -9,60 +9,12 @@
         require_once $className . '.php';
     });
 
-    // var_dump($_GET['uid']);
-
-    // if($_GET){
-    //     $sqluid = $_GET['uid'];
-    //     $sql = "SELECT * FROM `ordert` LEFT JOIN `orderdetails` ON ordert.orderId = orderdetails.orderId WHERE uId = '{$sqluid}'";
-    //     $result=$mysqli->query($sql);
-
-    //     $dataArray = array();
-    //     while($dataa = $result->fetch_object()){
-    //         $dataArray[] = $dataa;
-    //     }
-    //     // $dataa = $result->fetch_object();
-    //     $data = json_encode($dataArray);
-    //     echo ($data);
-    // }else{
-    //     echo "Nothing to get";
-    // }
-
-    // $input = file_get_contents("php://input");
-    // $output = json_decode($input);
-    // $sqlId = $output->menuItemId;
-
-    // var_dump($output);
-    
-    // var_dump($output);
-
-    // $data = array();
-    // $data = $_GET;
-
-    // var_dump($data);
-    
-
-
-
     $sql = "SELECT * FROM carts";
-
     $result = $mysqli->query($sql);
-    // $data = $result->fetch_object();
-
     $dataArray = array();
-
     while ($carts = $result->fetch_object()){
         $dataArray[] = $carts;
-        
-
-        // echo json_encode($carts);
-        // var_dump($carts);
     }
-    // $fData = json_encode($dataArray);
-    // echo $fData;
-
-
-
-
     $finalData = json_encode($dataArray);
     echo $finalData;
 
--- ChtProject/sendmail/sendmailTest2.php
+++ ChtProject/sendmail/sendmailTest2.php
@@ -2,8 +2,8 @@
 $to      = 'drama3fu@gmail.com';
 $subject = 'the subject';
 $message = 'hello';
-$headers = 'From: drama3fu@gmail.com' . "\r\n" .
-    'Reply-To: drama3fu@gmail.com' . "\r\n" .
+$headers = 'From: 順便外送平台' . "\r\n" .
+    'Reply-To: 順便外送平台' . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
 
 mail($to, $subject, $message, $headers);
